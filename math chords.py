f292b8b5be689a7551e51ec86ea0744a1810ce70
ashkon <164487760+ashkon-d@users.noreply.github.com>
2024-03-31 00:30:05 -0700

    Add files via upload

---
+++ math chords.py
@@ -0,0 +1,19 @@
+exponent=int(input("\nEnter the exponent for the function\n"))
+if (exponent>10):
+    print ("\nkys")
+x=0 #x variable
+y=0 #
+print ("\n")
+print("Pink Floyd: All Movement is accomplished in 6 stages")
+print("0")
+for x in range(8):
+    fo = x**exponent
+    #print (fo)
+    for y in range(fo):
+        if (fo<7):
+            print (fo)
+            break
+        fo-=7
+print("and the seventh brings return!")
+
+
